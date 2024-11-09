1fec46f9608b341bca7960af545071569a886033
Pozzitron1337 <the.vaho1337@gmail.com>
2024-11-09 04:12:04 +0200

    added standart deployment of TonA

--- scripts/deployTonA.ts
+++ scripts/deployTonA.ts
@@ -1,26 +1,30 @@
-import { toNano, Address } from '@ton/core';
+import { beginCell, contractAddress, toNano, TonClient4, WalletContractV5R1, internal, fromNano } from "@ton/ton";
+import { NetworkProvider } from "@ton/blueprint";
+import { Address } from "@ton/core";
+import { mnemonicToPrivateKey } from "@ton/crypto"
 import { TonA } from '../wrappers/TonA';
-import { NetworkProvider } from '@ton/blueprint';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+// $ npx blueprint run
 export async function run(provider: NetworkProvider) {
-    //tona owner address: EQA-hAveGp4YVY6HK05bAsO9lSxQ36tiPO5qWsPdDdS_WdUE
+    const ticketType = 0n;
     const tonaOwnerAddress = Address.parse('UQA-hAveGp4YVY6HK05bAsO9lSxQ36tiPO5qWsPdDdS_WYjB')
-    const tonA = provider.open(await TonA.fromInit(tonaOwnerAddress));
+    const tonA = provider.open(await TonA.fromInit(ticketType, tonaOwnerAddress));
 
     console.log(tonaOwnerAddress)
 
-    // await tonA.send(
-    //     provider.sender(),
-    //     {
-    //         value: toNano('0.05'),
-    //     },
-    //     {
-    //         $$type: 'Deploy',
-    //         queryId: 0n,
-    //     }
-    // );
+    await tonA.send(
+        provider.sender(),
+        {
+            value: toNano('0.05'),
+        },
+        {
+            $$type: 'Deploy',
+            queryId: 0n,
+        }
+    );
 
-    // await provider.waitForDeploy(tonA.address);
-
-    // run methods on `tonA`
+    await provider.waitForDeploy(tonA.address);
 }
